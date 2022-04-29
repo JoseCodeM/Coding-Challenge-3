b09c5c668102ca8c370bd62f664f3960270d9da5
Jose Eduardo Martinez <joseeduardomartinez@MacBook-Pro-3.local>
2022-04-29 14:24:30 -0400

    Added all JS Code

---
+++ script.js
@@ -0,0 +1,19 @@
+//1 Convert the following function into an arrow function named "arrowMyFunction".
+const arrowMyFunction = (num1,num2) => {return num1 + num2}; 
+//Testing
+console.log(arrowMyFunction(1,2)); //3
+
+
+//2 Convert the following function into a one-lined arrow function without a return statement 
+//(the return is implicit) . Name the function arrowAdd100
+const arrowAdd100 = money => money + 100;
+//Testing
+console.log(arrowAdd100(2)); //102
+
+
+//3 Create an arrow function that takes TWO arguments, a string and a single letter.  The 
+//function will count the number of times the specified letter occurs in the string. **Not case sensitive
+const hello = (str, letter) => str.split(letter).length - 1;
+//Testing
+console.log(hello("hello","l")); //2
+console.log(hello("my name is jose martinez", "m")); //3
